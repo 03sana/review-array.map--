2148462687f19f2e1be359f881023e979e410edb
sana <sana.nasani3@gmail.com>
2024-03-08 18:51:17 +0300

    answers

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 Challenge 1:
 Given an array of numbers, return an array of each number, squared
 */
-const nums = [1, 2, 3, 4, 5]
+const nums = [1, 2, 3, 4, 5];
 // -->       [1, 4, 9, 16, 25]
 // Your code here
-
-
-
+const squares = nums.map(function (num) {
+  return num * num;
+});
 
 /*
 Challenge 2:
@@ -15,12 +15,12 @@ Given an array of strings, return an array where
 the first letter of each string is capitalized
 */
 
-const names = ["alice", "bob", "charlie", "danielle"]
+const names = ["alice", "bob", "charlie", "danielle"];
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
-
-
-
+const capitalized = names.map((name) => {
+  return name[0].toUpperCase() + name.slice(1);
+});
 
 /*
 Challenge 3:
@@ -31,8 +31,10 @@ E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
 return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 */
 
-const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
 // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 // Your code here
 
-
+const paragraphs = pokemon.map((mon) => {
+  return `<p>${mon}</p>`;
+});
